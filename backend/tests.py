93d83574ba36cbfc9d14fba781333e39c65259e0
dmitryro <dmitryro@gmail.com>
2020-10-23 09:47:13 -0400

    backend

--- backend/tests.py
+++ backend/tests.py
@@ -2,7 +2,6 @@ from app import create_app
 from app.config import test_config
 from app.database import db
 from app.user.models import User
-from sqlalchemy.sql.expression import func
 from faker import Faker
 import unittest
 
